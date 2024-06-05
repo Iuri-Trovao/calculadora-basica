7462017647b6e9220d8ae642d676bda8413b9bd0
iuri lourenço trovão <114348640+Iuri-Trovao@users.noreply.github.com>
2024-06-05 17:20:37 -0300

    new path

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # calculadora-basica
 calculadora basica para treinar JAVASCRIPT
+
+# fundo do corpo
+ linear-gradient(to bottom, rgb(218, 216, 216), rgb(56, 56, 56));
+
+# fundo do menu
+linear-gradient(to bottom, rgb(82, 41, 102), rgb(65, 34, 80), rgb(37, 24, 44));
+
+# cor do botão
+background: rgb(116, 45, 151);
+
+# palheta do hover do botão
+background: rgb(111, 53, 140);
+box-shadow: 10px 10px 100px rgb(195, 118, 234);
--- calculadora.js
+++ calculadora.js
@@ -1,27 +1,18 @@
-alert('seja bem vindo!!!')
-
-function calcular() {
-    let input1 = document.getElementById('input1')
-    let input2 = document.getElementById('input2')
-    let n1 = Number(input1.value)
-    let n2 = Number(input2.value)
-    let resposta = document.getElementById('res') 
-    let selecao = document.getElementById('select').value
-    let soma = n1 + n2
-    let menos = n1 - n2
-    let multi = n1 * n2
-    let dividir = n1 / n2
-
-    if(selecao === 'somar'){
-        resposta.innerHTML = `a resposta é ${soma}`
-    }else if(selecao === 'menos'){
-        resposta.innerHTML = `a resposta é ${menos}`
-    }else if(selecao === 'multiplicar'){
-        resposta.innerHTML = `a resposta é ${multi}`
-    }else if(selecao === 'dividir'){
-        resposta.innerHTML = `a resposta é ${dividir}`
+const Button = document.getElementById('but')
+Button.addEventListener('click', ()=>{
+    const input1 = document.getElementById('input1')
+    const input2 = document.getElementById('input2')
+    const valor1 = Number(input1.value) 
+    const valor2 = Number(input2.value) 
+    const Selecao = document.getElementById('select').value
+    const res = document.getElementById('res')
+    if(Selecao === 'somar'){
+        res.innerHTML = `${valor1} + ${valor2} = ${valor1 + valor2}`;
+    }else if(Selecao === 'menos'){
+        res.innerHTML = `${valor1} + ${valor2} = ${valor1 - valor2}`;
+    }else if(Selecao === 'multiplicar'){
+        res.innerHTML = `${valor1} + ${valor2} = ${valor1 * valor2}`;
+    }else if(Selecao === 'dividir'){
+        res.innerHTML = `${valor1} + ${valor2} = ${valor1 / valor2}`;
     }
-}
-
-let button = document.getElementById('but')
-button.onclick = calcular()
+})
